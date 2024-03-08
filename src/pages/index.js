f34b8c397b401710897938d9c41de11c64ef1b1a
Prince Pal <princepal9120@gmail.com>
2024-03-08 16:05:50 +0530

    depdeancy

--- src/pages/index.js
+++ src/pages/index.js
@@ -66,7 +66,7 @@ export default function Home() {
     if (fileInput) {
       fileInput.addEventListener("change", onChangeFileInput);
     }
-  }, []);
+  }, [onChangeFileInput]);
 
   return (
     <main
